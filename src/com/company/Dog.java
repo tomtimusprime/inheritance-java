3084bf25ca196678eb4edb627b9dd2f6fa6e13e2
Tom Black <tom.black06007@gmail.com>
2021-04-22 21:59:15 -0700

    Working with inheritance in Java.

---
+++ src/com/company/Dog.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Dog extends Animal{
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int brain, int body, int size, int weight) {
+        super(name, brain, body, size, weight);
+    }
+}
